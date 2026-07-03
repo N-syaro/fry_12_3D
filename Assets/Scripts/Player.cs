eb3d60dd85fb44dbcae216395871a76bd05347c8
ibuki-27 <haru.terada78@gmail.com>
2026-07-03 12:40:19 +0900

    2gr

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,19 +5,29 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float speed;
     PlayerInput playerInput;
+    [SerializeField] float accel;
+    Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         playerInput = GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        var MoveVec = playerInput.actions["Move"].ReadValue<Vector2>();
-        var MoveVec3D = new Vector3(MoveVec.x*speed,0,MoveVec.y*speed);
-        transform.position = transform.position + MoveVec3D * Time.deltaTime;
-        transform.position = transform.position + MoveVec3D * Time.deltaTime;
-        Debug.Log(MoveVec);
+        var accelVec = playerInput.actions["Move"].ReadValue<Vector2>();
+
+        var cameraDir = playerInput.camera.transform.forward;
+        cameraDir.y = 0;
+        cameraDir = cameraDir.normalized;
+
+        var cameraRight = playerInput.camera.transform.right;
+
+        var accelVec3D =
+            cameraDir * accelVec.y * accel
+            + cameraRight * accelVec.x * accel;
+        rb.AddForce(accelVec3D, ForceMode.Acceleration);
     }
 }
